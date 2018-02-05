3834857aac9b347039a0e391293cc0bcaa443adf
SoOnBlitz <34095585+SoOnBlitz@users.noreply.github.com>
2018-02-05 15:38:52 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
+	<link href='http://fonts.which is googleapis.com/css?family=Roboto:400,300,300italic,500,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="//storage.googleapis.com/code.getmdl.io/1.0.1/material.teal-red.min.css" />
+	<script src="//storage.googleapis.com/code.getmdl.io/1.0.1/material.min.js"></script>
+	<meta charset="UTF-8">
+	<title>DANGERS SUR LE RÉGIME VEGAN</title>
+</head>
+<body>
+<div class="home">
+<center><h1>DANGERS DU RÉGIME VÉGAN</h1></center>
+<img src="images/regime.png" alt="" />
+<h1>Une mode assez récente</h1>
+Le véganisme, également appelé "véganisme intégral est un mode de vie qui consiste à ne consommer aucun produit issu des animaux ou de leur exploitation.
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu du site</title>
+	<meta charset=="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1"
+	<link rel="stylesheet" href="menustyle.css">
+</head>
+<body>
+	<nav>
+		<label for="menu-mobile" class="menu-mobile">Menu</label>
+		<input type="checkbox" id="menu-mobile" role="button"
+		<ul>
+			<li class="menu-accueil"><a href="#">Accueil</a>
+			</li>
+			<li class="menu-page2"><a href="#">Page2</a>
+			</li>
+			<li class="menu-page3"><a href="#">Page3</a>
+			</li>
+			<li class="menu-contact"><a href="#">Contact</a>
+			</li>
+		</ul>
+	</nav>
+</body>
+</html>
